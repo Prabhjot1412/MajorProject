f6045fe74e026bb4cdc4ea9217982212b4fa37f0
sbhosale-cemtrexlabs <sbhosale@cemtrexlabs.com>
2022-10-21 01:37:18 +0530

    customizing devise pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :patient, class_name: 'DoctorPatient'
-  has_one :doctor, class_name: 'DoctorPatient'
-
+  has_many :patient, class_name: 'DoctorPatient', foreign_key: 'patient_id'
+  has_many :doctor, class_name: 'DoctorPatient', foreign_key: 'doctor_id'
+  
+  enum :user_type, %i[patient doctor admin], default: 'patient'
 end
